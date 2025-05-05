c0238a9e8d7e491aa15113e3d956dbe825e12be8
Bossou Chiegueu Aryl Shamir <arylshamir98@gmail.com>
2025-05-05 06:47:45 +0100

    joined version

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+from art import logo
+print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number: \n"))
 
-
-def encode(original_text, shift_amount):
+def ceaser(original_text, shift_amount, encode_or_decode):
     cipher_text = ""
+    if encode_or_decode == 'decode':
+        shift_amount *= -1
+
     for letter in original_text:
         if letter in alphabet:
             position = alphabet.index(letter)
@@ -19,13 +20,19 @@ def encode(original_text, shift_amount):
     print(f'Here is your encoded result {cipher_text}')
 
 
-def decode(original_text, shift_amount):
-    cipher_text = ""
-    for letter in original_text:
-        if letter in alphabet:
-            position = alphabet.index(letter)
-            new_position = (position - shift_amount) % len(alphabet)
-            cipher_text += alphabet[new_position]
-        else:
-            cipher_text += letter
-    print(f'Here is your decoded result {cipher_text}')
+should_continue = True
+
+while should_continue:
+    
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number: \n"))
+
+    ceaser(text, shift, direction)
+    restart = input("Type 'yes' if you want to go again, otherwise type 'no'").lower()
+
+    if restart == 'no':
+        should_continue = False
+        print('Good bye, see you next time.')
+
+
